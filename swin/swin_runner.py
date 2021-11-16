994376f8615744c223f8e7e0edafdd6e0c7388a1
th-br <th-braams@t-online.de>
2021-11-16 21:53:43 +0100

    fix: refactored train test path

--- swin/swin_runner.py
+++ swin/swin_runner.py
@@ -3,12 +3,12 @@ import swin
 import os
 
 #%%
-st = swin.PetfinderTransformer(seed=999,dataset_parent="../petfinder-pawpularity-score",
-              train_test_json_path="train_test_val.json")
+st = swin.PetfinderTransformer(seed=1337, dataset_parent="../petfinder-pawpularity-score",
+                               train_test_json_path="../petfinder-pawpularity-score/train_test_val.json")
 
 #%%
 
 st.wrap_model(dataset_parent="../petfinder-pawpularity-score",
-              train_test_json_path="train_test_val.json",
+              train_test_json_path="../petfinder-pawpularity-score/train_test_val.json",
               batch_size=4, image_size=224,
               model_name="swin_large_patch4_window7_224", epochs=2)
